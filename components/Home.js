88c97f4fbb40775b16eee5aa224897137bbfc5dc
Damien <damien.mourot.91@gmail.com>
2024-11-13 12:40:52 +0100

    Sauvegarde changement d'urls fetch

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('https://mymoviz5-backend-new.vercel.app/movies')
       .then(response => response.json())
       .then(data => {
         console.log(data);
